3e824dd3a39126834224009890cb1b739cf3b9d6
avishkarox <avishka.madushanka123@gmail.com>
2023-07-20 12:25:14 +0530

    changes

--- rmfrontend/src/scenes/team/index.jsx
+++ rmfrontend/src/scenes/team/index.jsx
@@ -255,7 +255,7 @@ const Team = () => {
       }
 
       <Dialog open={openFormDialog} onClose={() => setOpenFormDialog(false)}>
-        <DialogTitle>Edit User</DialogTitle>
+        <DialogTitle>Edit User Details</DialogTitle>
         <DialogContent>
           <Formik
             initialValues={selectedUser || {}}
