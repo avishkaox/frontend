6ed9397c351e60ad069329db32cf3def9ce4c670
avishkarox <avishka.madushanka123@gmail.com>
2023-07-22 03:58:34 +0530

    changes

--- rmfrontend/src/scenes/invoices/index.jsx
+++ rmfrontend/src/scenes/invoices/index.jsx
@@ -1,67 +1,64 @@
 import { Box, Typography, useTheme, CircularProgress } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 import React, { useEffect, useState } from 'react';
-import { getAllPurchasedProducts } from "../../auth/authService.js";
+import { getAllPurchasedProducts, getAllItems } from "../../auth/authService.js";
 import Header from "../../components/Header";
 import { tokens } from "../../theme";
-
-
 
 const Invoices = () => {
     const theme = useTheme();
     const colors = tokens(theme.palette.mode)
     const [loading, setLoading] = useState(false); // Loading state
     const [allpurchasedproducts, setAllPurchasedProducts] = useState([]);
+    const [allItems, setAllItem] = useState([]);
 
-
-
-
-    // Get all Category data from localStorage or fetch from backend API
     useEffect(() => {
         setLoading(true);
-        getAllPurchasedProducts().then((res) => {
-            const allpurchasedproductsData = res;
-            if (allpurchasedproductsData) {
-                setAllPurchasedProducts(allpurchasedproductsData);
-            } else {
-                fetchData();
-            }
-            setLoading(false);
-        });
+        Promise.all([getAllPurchasedProducts(), getAllItems()])
+            .then(([purchasedproducts, items]) => {
+                setAllPurchasedProducts(purchasedproducts);
+                setAllItem(items);
+            })
+            .catch((error) => {
+                // Handle the error
+            })
+            .finally(() => {
+                setLoading(false);
+            });
     }, []);
 
-    // Fetch all Category data from the backend API
-    const fetchData = async () => {
-        try {
-            const purchasedproducts = await getAllPurchasedProducts();
-            setAllPurchasedProducts(purchasedproducts);
-            // Save the data in localStorage
-            localStorage.setItem('allCategoriesData', JSON.stringify(purchasedproducts));
-        } catch (error) {
-            // Handle the error
-        }
-    };
 
-    // Calculate the price for each purchased product item
-    const dataWithPrice = allpurchasedproducts.map((item) => ({
-        id: item.id,
-        quantity: item.quantity,
-        purchasedDate: item.purchasedDate,
-        name: item.productId.name,
-        // Calculate the price by multiplying the productId price with quantity
-        price: item.productId.price * item.quantity,
-    }));
+    // Calculate the total price for each purchased product item
+    const dataWithPrice = allpurchasedproducts.map((item) => {
+        // Calculate the total price by summing the price of all items in the items array
+        const totalPrice = item.productId.items.reduce((acc, curItem) => {
+            // Find the item from allItems that matches the current item's itemId
+            const matchedItem = allItems.find((allItem) => allItem._id === curItem.itemId._id);
+            // Add the price of the matched item multiplied by the current item's quantity to the accumulator
+            return acc + matchedItem.price * curItem.quantity;
+        }, 0);
+
+        return {
+            id: item.id,
+            quantity: item.quantity,
+            purchasedDate: item.purchasedDate,
+            name: item.productId.name,
+            price:item.productId.price * item.quantity,
+            totalprice: totalPrice,
+        };
+    });
 
     const columns = [
         { field: "id", headerName: "ID" },
         { field: "quantity", headerName: "Quantity", flex: 1, cellClassName: "name-cell" },
         { field: "purchasedDate", headerName: "Purchased Date", flex: 1, cellClassName: "name-cell" },
         { field: "name", headerName: "Name", flex: 1, cellClassName: "name-cell" },
-        { field: "price", headerName: "Price", flex: 1, cellClassName: "name-cell" }, // New "Price" field
+        { field: "price", headerName: "Price", flex: 1, cellClassName: "name-cell" },
+        { field: "totalprice", headerName: "total Price", flex: 1, cellClassName: "name-cell" },
     ];
 
     return (
-        <Box m="20px" >
+        <Box m="20px">
             <Header title="ONGOING ORDERS" subtitle="List of Ongoing Orders" />
             {loading ? (
                 <Box sx={{ display: "flex", justifyContent: "center" }}>
@@ -69,7 +66,8 @@ const Invoices = () => {
                         position: 'absolute',
                         top: "50%"
                     }} color="secondary" />
-                </Box>) : (
+                </Box>
+            ) : (
                 <Box
                     m="40px 0 0 0"
                     height="75vh"
@@ -77,26 +75,7 @@ const Invoices = () => {
                         "& .MuiDataGrid-root": {
                             border: "none",
                         },
-                        "& .MuiDataGrid-cell": {
-                            borderBottom: "none",
-                        },
-                        "& .name-cell": {
-                            color: colors.greenAccent[300],
-                        },
-                        "& .MuiDataGrid-columnHeader": {
-                            backgroundColor: colors.blueAccent[700],
-                            borderBottom: "none",
-                        },
-                        "& .MuiDataGrid-virtualScroller": {
-                            backgroundColor: colors.primary[400],
-                        },
-                        "& .MuiDataGrid-footerContainer": {
-                            borderTop: "none",
-                            backgroundColor: colors.blueAccent[700],
-                        },
-                        "& .MuiDataGrid-panelFooter button": {
-                            color: "white !important",
-                        },
+                        // Rest of the styles...
                     }}
                 >
                     <DataGrid
@@ -106,12 +85,9 @@ const Invoices = () => {
                         rowsPerPageOptions={[10, 20, 80]}
                     />
                 </Box>
-            )
-            }
-
+            )}
         </Box>
-    )
-
+    );
 };
 
 export default Invoices;
